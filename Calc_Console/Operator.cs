b553508fef555195f9df793604c076a51d1fa097
GDC6 <GDC6@DESKTOP-VVDCONR>
2020-09-25 13:25:48 +0900

    초기 커밋.

---
+++ Calc_Console/Operator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc_Console
+{
+    class Operator //사칙연산 메서드
+    {
+
+    }
+}
---
+++ Calc_Console/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("===============계산기====================");
+            Console.WriteLine("난이도: 1. 상 \t 2. 중 \t 3. 하");
+            Console.WriteLine("=========================================");
+
+            while (true)
+            {
+                Random rNum = new Random();
+                rNum.Next(10, 101);
+                Random rOper = new Random();
+                rOper.Next(1, 5); // 1= '+' , 2= '-', 3= '*', 4= '/'
+
+                Console.Write($"{rNum} {rOper} {rNum} =");
+                int result = int.Parse(Console.ReadLine());
+
+
+            }
+        }
+    }
+}
